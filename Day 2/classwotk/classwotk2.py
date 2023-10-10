33e24377d889aaa6077f33a77a1130c40ac338ce
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-10 20:29:12 +0400

    dis is classwotk2 and homewotk2
    cya also i added smth in homewotk2 you will see

---
+++ Day 2/classwotk/classwotk2.py
@@ -0,0 +1,24 @@
+ #PC_pasi = 10000
+ #discount = 10 #%discount
+ #vaible_money=9600
+
+ #how much left???
+
+ #print(avaible_money - PC_pasi*(100-discount)/100)
+
+ #so if u see this i will skip smth cause i  know what it means yeah... nah u thought!!!!!
+
+#print("nikolozis book")
+
+#print(7 + 3)
+#print(10 - 5)
+#print(10 / 2)
+#print(16 // 2)
+#print(23 % 5)
+
+#budget = 200
+
+#print(budget)
+
+
+
---
+++ Day 2/homewotk/homewotk2.py
@@ -0,0 +1,19 @@
+
+
+Dadsage=39
+
+nikolozage=13
+
+import time
+
+while True:
+   print("after 1 second")
+   time.sleep(1)
+
+   print(Dadsage+nikolozage)
+
+   
+
+
+
+        
